bd1926766aa3e525ae7ae46b1c46094eac2f6307
hermann EHO <hermannleboss@gmail.com>
2020-10-13 20:17:03 +0000

    management of images and add of loading

--- Components/Search.js
+++ Components/Search.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {StyleSheet, View, Button, TextInput, FlatList, Text} from 'react-native'
+import {StyleSheet, View, Button, TextInput, FlatList, Text, ActivityIndicator } from 'react-native'
 import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
 import {getFilmsFromApiWithSearchedText} from '../API/TMDBApi'
@@ -9,22 +9,40 @@ class Search extends Component {
   constructor(props) {
    super(props)
    this.state = {
-     films:[]
+     films:[],
+     isLoading: false,
    }
    this.serachedText=""
  }
   _loadFilms() {
+    this.setState({isLoading: true})
     if (this.serachedText.length > 0) {
-      getFilmsFromApiWithSearchedText(this.serachedText).then(data => this.setState({films: data.results}));
+      getFilmsFromApiWithSearchedText(this.serachedText).then(data =>
+        this.setState({
+          films: data.results,
+          isLoading:false
+        })
+      );
     }
   }
   _searchTextInputChanged(text){
     this.serachedText = text
   }
+  _displayLoading() {
+      if (this.state.isLoading) {
+        return (
+          <View style={styles.loading_container}>
+            <ActivityIndicator size='large' />
+            {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+          </View>
+        )
+      }
+    }
     render() {
+      console.log(this.state.isLoading );
         return (
             <View style={styles.main_container}>
-                <TextInput onChangeText={(text)=> this._searchTextInputChanged(text)} style={styles.textinput} placeholder='Titre du film'/>
+                <TextInput onSubmitEditing={()=>this._loadFilms()} onChangeText={(text)=> this._searchTextInputChanged(text)} style={styles.textinput} placeholder='Titre du film'/>
                 <Button title='Rechercher' onPress={() => this._loadFilms()}/>
                 {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList */}
                 <FlatList
@@ -32,12 +50,17 @@ class Search extends Component {
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) =>  <FilmItem film={item}/>}
                 />
+                {this._displayLoading()}
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+  main_container:{
+    marginTop:20,
+    flex: 1
+  },
     textinput: {
         marginLeft: 5,
         marginRight: 5,
@@ -45,6 +68,15 @@ const styles = StyleSheet.create({
         borderColor: '#000000',
         borderWidth: 1,
         paddingLeft: 5
+    },
+    loading_container: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 100,
+      bottom: 0,
+      alignItems: 'center',
+      justifyContent: 'center'
     }
 })
 
