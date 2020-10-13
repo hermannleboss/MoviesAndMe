2827351f08f8a3bcdd18f91d4be29402c9668ed7
hermann EHO <hermannleboss@gmail.com>
2020-10-13 19:45:42 +0000

    init of TMDB api

---
+++ API/TMDBApi.js
@@ -0,0 +1,10 @@
+// API/TMDBApi.js
+
+const API_TOKEN = "2cc0c850f78c27267d6ce9bde9ee16ce";
+
+export function getFilmsFromApiWithSearchedText (text) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+}
--- Components/Search.js
+++ Components/Search.js
@@ -2,19 +2,35 @@ import React, {Component} from 'react'
 import {StyleSheet, View, Button, TextInput, FlatList, Text} from 'react-native'
 import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
+import {getFilmsFromApiWithSearchedText} from '../API/TMDBApi'
 
 class Search extends Component {
+
+  constructor(props) {
+   super(props)
+   this.state = {
+     films:[]
+   }
+   this.serachedText=""
+ }
+  _loadFilms() {
+    if (this.serachedText.length > 0) {
+      getFilmsFromApiWithSearchedText(this.serachedText).then(data => this.setState({films: data.results}));
+    }
+  }
+  _searchTextInputChanged(text){
+    this.serachedText = text
+  }
     render() {
         return (
             <View style={styles.main_container}>
-                <TextInput style={styles.textinput} placeholder='Titre du film'/>
-                <Button title='Rechercher' onPress={() => {
-                }}/>
+                <TextInput onChangeText={(text)=> this._searchTextInputChanged(text)} style={styles.textinput} placeholder='Titre du film'/>
+                <Button title='Rechercher' onPress={() => this._loadFilms()}/>
                 {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList */}
                 <FlatList
-                    data={films}
+                    data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
-                    renderItem={({item}) =>  <FilmItem/>}
+                    renderItem={({item}) =>  <FilmItem film={item}/>}
                 />
             </View>
         )
