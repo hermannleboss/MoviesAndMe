97d754bfba165ba6aac863dcb22be66e6ee7f3d8
hermann EHO <hermannleboss@gmail.com>
2020-10-13 14:58:34 +0000

    add fimls items to search and refactor

--- Components/Search.js
+++ Components/Search.js
@@ -1,16 +1,26 @@
 import React, {Component} from 'react'
-import {StyleSheet, View, Button, TextInput} from 'react-native'
+import {StyleSheet, View, Button, TextInput, FlatList, Text} from 'react-native'
+import films from '../Helpers/filmsData'
+import FilmItem from './FilmItem'
 
 class Search extends Component {
     render() {
         return (
-            <View style={{marginTop: 20}}>
-                <TextInput  style={styles.textinput} placeholder="Titre du film"/>
-                <Button title="Rechercher" onPress={()=>{}}/>
+            <View style={styles.main_container}>
+                <TextInput style={styles.textinput} placeholder='Titre du film'/>
+                <Button title='Rechercher' onPress={() => {
+                }}/>
+                {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList */}
+                <FlatList
+                    data={films}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) =>  <FilmItem/>}
+                />
             </View>
         )
     }
 }
+
 const styles = StyleSheet.create({
     textinput: {
         marginLeft: 5,
