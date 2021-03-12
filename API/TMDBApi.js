3c17397011fb096120c7b24b0c845fe58224c7cf
EHO K. Hermann <hermannleboss@gmail.com>
2021-03-12 19:40:04 +0000

    film detail : add loading

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -2,13 +2,20 @@
 
 const API_TOKEN = "2cc0c850f78c27267d6ce9bde9ee16ce";
 
-export function getFilmsFromApiWithSearchedText (text, page) {
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + '&page=' + page
-  return fetch(url)
-    .then((response) => response.json())
-    .catch((error) => console.error(error))
+export function getFilmsFromApiWithSearchedText(text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + '&page=' + page
+    return fetch(url)
+            .then((response) => response.json())
+            .catch((error) => console.error(error))
 }
 
-export function getImageFromApi(name){
-  return 'https://image.tmdb.org/t/p/w300' + name
+export function getImageFromApi(name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+}
+
+// Récupération du détail d'un film
+export function getFilmDetailFromApi(id) {
+    return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+            .then((response) => response.json())
+            .catch((error) => console.error(error));
 }
--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,15 +1,34 @@
 // Components/FilmDetail.js
 
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text , ActivityIndicator} from 'react-native'
 
 class FilmDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      film: undefined,
+      isLoading: true
+    }
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      // Si isLoading vaut true, on affiche le chargement à l'écran
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
+    }
+  }
+
   render() {
 
-    const idFilm=this.props.route.params.idFilm
+    const idFilm = this.props.route.params.idFilm
     return (
       <View style={styles.main_container}>
-        <Text>Détail du film {idFilm}</Text>
+      {this._displayLoading()}
       </View>
     )
   }
@@ -18,6 +37,15 @@ class FilmDetail extends React.Component {
 const styles = StyleSheet.create({
   main_container: {
     flex: 1,
+  },
+  loading_container: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      alignItems: 'center',
+      justifyContent: 'center'
   }
 })
 
