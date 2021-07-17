55704afa009fbc1c98d16159ea3a4f6982a2f02a
EHO K. Hermann <hermannleboss@gmail.com>
2021-07-17 22:50:08 +0000

    Bottom Tab Navigation

--- Components/Search.js
+++ Components/Search.js
@@ -9,7 +9,7 @@ import {
   ActivityIndicator,
 } from "react-native";
 import films from "../Helpers/filmsData";
-import FilmItem from "./FilmItem";
+import FilmList from "./FilmList";
 import { getFilmsFromApiWithSearchedText } from "../API/TMDBApi";
 
 import { connect } from "react-redux";
@@ -24,9 +24,10 @@ class Search extends Component {
       films: [],
       isLoading: false,
     };
+    //this._loadFilms=this._loadFilms.bind(this)
   }
 
-  _loadFilms() {
+  _loadFilms = () => {
     if (this.serachedText.length > 0) {
       this.setState({ isLoading: true });
       getFilmsFromApiWithSearchedText(this.serachedText, this.page + 1).then(
@@ -41,7 +42,7 @@ class Search extends Component {
         }
       );
     }
-  }
+  };
 
   _searchTextInputChanged(text) {
     this.serachedText = text;
@@ -84,33 +85,12 @@ class Search extends Component {
           placeholder="Titre du film"
         />
         <Button title="Rechercher" onPress={() => this._searchFilms()} />
-        {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList */}
-        <FlatList
-          data={this.state.films}
-          //J'ai ajouter key={data} pour résoudre ce problème: Encountered two children with the same key
-          key={data}
-          extraData={this.props.favoritesFilm}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <FilmItem
-              film={item}
-              //J'ai ajouter key={data} pour résoudre ce problème: Encountered two children with the same key
-              //key={item}
-              isFilmFavorite={
-                (this.props.favoritesFilm.findIndex(
-                  (film) => film.id === item.id
-                ) !== -1) ?true: false
-              }
-              keyExtractor={(item2) => item.title.toString()}
-              displayDetailForFilm={this._displayDetailForFilm}
-            />
-          )}
-          onEndReachedThreshold={0.5}
-          onEndReached={() => {
-            if (this.page < this.totalPages) {
-              this._loadFilms();
-            }
-          }}
+        <FilmList
+          films={this.state.films} // C'est bien le component Search qui récupère les films depuis l'API et on les transmet ici pour que le component FilmList les affiche
+          navigation={this.props.navigation} // Ici on transmet les informations de navigation pour permettre au component FilmList de naviguer vers le détail d'un film
+          loadFilms={this._loadFilms} // _loadFilm charge les films suivants, ça concerne l'API, le component FilmList va juste appeler cette méthode quand l'utilisateur aura parcouru tous les films et c'est le component Search qui lui fournira les films suivants
+          page={this.page}
+          totalPages={this.totalPages} // les infos page et totalPages vont être utile, côté component FilmList, pour ne pas déclencher l'évènement pour charger plus de film si on a atteint la dernière page
         />
         {this._displayLoading()}
       </View>
@@ -139,11 +119,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-})
+});
 
 const mapStateToProps = (state) => {
   return {
     favoritesFilm: state.favoritesFilm,
-  }
-}
+  };
+};
 export default connect(mapStateToProps)(Search);
--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,34 +1,81 @@
 // Navigation/Navigation.js
 
 //import {createAppContainer } from 'react-navigation'
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import {Button, View, Text, Image} from 'react-native'
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
 
-import Search from '../Components/Search'
-import FilmDetail from '../Components/FilmDetail'
-
+import Search from "../Components/Search";
+import FilmDetail from "../Components/FilmDetail";
+import Favorites from "../Components/Favorites";
 
 const Stack = createStackNavigator();
 const SearchStackNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Search"
-          component={Search}
-          options={{ title: 'Recherche' }}
-        />
-        <Stack.Screen
-          name="FilmDetail"
-          component={FilmDetail}
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Search"
+        component={Search}
+        options={{ title: "Recherche" }}
+      />
+      <Stack.Screen name="FilmDetail" component={FilmDetail} />
+    </Stack.Navigator>
+  );
+};
+const Tab = createBottomTabNavigator();
 
+const MoviesTabNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        tabBarOptions={{
+          showLabel: false,
+          showIcon: true,
+          activeBackgroundColor: "#DDDDDD",
+          inactiveBackgroundColor: '#FFFFFF'
+        }}
+      >
+        <Tab.Screen
+          name="Search"
+          component={SearchStackNavigator}
+          options={{
+            tabBarIcon: () => {
+              return (
+                <Image
+                  source={require("../Images/ic_search.png")}
+                  style={styles.icon}
+                />
+              );
+            },
+          }}
         />
-      </Stack.Navigator>
+        <Tab.Screen
+          name="Favoris"
+          component={Favorites}
+          options={{
+            tabBarIcon: () => {
+              return (
+                <Image
+                  source={require("../Images/ic_favorite.png")}
+                  style={styles.icon}
+                />
+              );
+            },
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
 
-export default SearchStackNavigator;
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30,
+  },
+});
+
+export default MoviesTabNavigator;
